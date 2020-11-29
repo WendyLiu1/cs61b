6837d043b7fa97f74ca9243e37398dd61c4a9264
WendyLiu1 <wendyjwliu@gmail.com>
2020-11-29 15:28:21 -0600

    remove clab6

--- lab6/BubbleGrid.java
+++ lab6/BubbleGrid.java
@@ -1,10 +1,18 @@
 public class BubbleGrid {
     private final int[][] bubbleGrid;
+    /** Create new BubbleGrid with bubble/space locations specified by grid.
+     * Grid is composed of only 1's and 0's, where 1's denote a bubble, and
+     * 0's denote a space.
+     */
     public BubbleGrid(int[][] grid) {
         this.bubbleGrid = grid;
     }
 
     /**
+     * Returns an array whose i-th element is the number of bubbles that
+     * fall after the i-th dart is thrown. Assume all elements of darts
+     * are unique, valid locations in the grid. Must be non-destructive
+     * and have no side-effects to grid.
      * @param darts and array of 2-element arrays representing the grid positions
      *              (in [row, col] format) at which darts are thrown in sequence(i.e.
      *              a dart is thrown at position darts[t] at time t).
