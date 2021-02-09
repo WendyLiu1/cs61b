caa9e9b11213714081cd197d1b7d7bf477b1aa5d
WendyLiu1 <wendyjwliu@gmail.com>
2021-02-08 21:46:12 -0600

    hw2 done, first commit

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,119 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private int n;
+    private int[][] grid;
+    private WeightedQuickUnionUF uf;
+    private int numOpenSite;
+    private int topIdx;
+    private boolean perculates;
+    // create N-by-N grid, with all sites initially blocked
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new IllegalArgumentException("N must be larger than 0");
+        }
+        this.n = N;
+        // the last one is used to indicate it's top row
+        this.uf = new WeightedQuickUnionUF(N * N + 1);
+        this.topIdx = N * N;
+        this.grid = new int[N][N];
+        this.numOpenSite = 0;
+        this.perculates = false;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < n; j++) {
+                this.grid[i][j] = 0;
+            }
+        }
+    }
 
+    /**
+     * open the site (row, col) if it is not open already
+     * @param row
+     * @param col
+     */
+    public void open(int row, int col) {
+        this.inputCheck(row);
+        this.inputCheck(col);
+        if (this.grid[row][col] == 0) {
+            int idx = this.convert2dTo1d(row, col);
+            this.grid[row][col] = 1;
+            if (row > 0 && this.isOpen(row - 1, col)) {
+                this.uf.union(idx, this.convert2dTo1d(row - 1, col));
+            }
+            if (row < this.n - 1 && this.isOpen(row + 1, col)) {
+                this.uf.union(idx, this.convert2dTo1d(row + 1, col));
+            }
+            if (col > 0 && this.isOpen(row, col - 1)) {
+                this.uf.union(idx, this.convert2dTo1d(row, col - 1));
+            }
+            if (col < this.n - 1 && this.isOpen(row, col + 1)) {
+                this.uf.union(idx, this.convert2dTo1d(row, col + 1));
+            }
+            if (row == 0) {
+                this.uf.union(idx, this.topIdx);
+            }
+            if (!this.perculates && row == this.n - 1) {
+                this.perculates = this.uf.connected(idx, this.topIdx);
+            }
+            this.numOpenSite++;
+        }
+    }
+
+    /**
+     * is the site (row, col) open?
+     * @param row
+     * @param col
+     * @return
+     */
+    public boolean isOpen(int row, int col) {
+        this.inputCheck(row);
+        this.inputCheck(col);
+        return this.grid[row][col] == 1;
+    }
+
+    /**
+     * is the site (row, col) full?
+     * @param row
+     * @param col
+     * @return
+     */
+    public boolean isFull(int row, int col) {
+        this.inputCheck(row);
+        this.inputCheck(col);
+        return this.uf.connected(this.convert2dTo1d(row, col), this.topIdx);
+    }
+
+    /**
+     * @return number of open sites
+     */
+    public int numberOfOpenSites() {
+        return this.numOpenSite;
+    }
+
+    /**
+     * does the system percolate?
+     * @return
+     */
+    public boolean percolates() {
+        return this.perculates;
+    }
+
+    /**
+     * check if the input is valid or not
+     */
+    private void inputCheck(int idx) {
+        if (idx < 0 || idx >= this.n) {
+            throw new IndexOutOfBoundsException("input is out of bound");
+        }
+    }
+
+    /**
+     * Convert 2d index to 1d
+     * @param row row number
+     * @param col col number
+     * @return return 1d index
+     */
+    private int convert2dTo1d(int row, int col) {
+        return col + row * this.n;
+    }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,57 @@
 package hw2;
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
 
 public class PercolationStats {
+    private double[] results;
+    /**
+     * Perform T independent experiments on an N-by-N grid
+     * @param N
+     * @param T
+     * @param pf
+     */
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException("Illegal input");
+        }
+        this.results = new double[T];
+        for (int i = 0; i < T; i++) {
+            this.results[i] = percolateSimulation(N, pf);
+        }
+    }
 
+    /**
+     * Perform 1 run on N-by-N grid
+     * @param N
+     * @param pf
+     * @return
+     */
+    private double percolateSimulation(int N, PercolationFactory pf) {
+        Percolation p = pf.make(N);
+        int numGrid = N * N;
+        while (!p.percolates()) {
+            int idx = StdRandom.uniform(numGrid);
+            int row = idx / N;
+            int col = idx % N;
+            p.open(row, col);
+        }
+        return (double) p.numberOfOpenSites() / (double) N;
+    }
+
+
+    public double mean() {
+        return StdStats.mean(this.results);
+    }
+
+    public double stddev() {
+        return StdStats.stddev(this.results);
+    }
+
+    public double confidenceLow() {
+        return this.mean() - 1.96 * this.stddev() / Math.sqrt(this.results.length);
+    }
+
+    public double confidenceHigh() {
+        return this.mean() + 1.96 * this.stddev() / Math.sqrt(this.results.length);
+    }
 }
